d44cd95538ecd6571eb7370ce6538d4dde775b46
Harshit Nagar <156645072+harshitnagar22@users.noreply.github.com>
2025-03-03 21:34:07 +0530

    Update component.rb

--- app/components/vitrail/card/component.rb
+++ app/components/vitrail/card/component.rb
@@ -4,6 +4,13 @@ module Vitrail
       renders_one :title
       renders_one :description
       renders_one :footer
+
+      # Add an attribute to accept an optional link
+      def initialize(link_to: nil)
+        @link_to = link_to
+      end
+
+      attr_reader :link_to
     end
   end
 end
